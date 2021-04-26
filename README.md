9b5e811c12aca2ca6be087ed52c87e525809e1bb
komoto <komoto@jeol.co.jp>
2021-04-26 11:09:16 +0900

    Add doc:str to the main function
    + doc:str added
    + edit readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-world
-Test
+
+Test 用レポジトリです．
--- sample.py
+++ sample.py
@@ -3,9 +3,12 @@
 
 
 def 人生と宇宙そしてすべての答え ():
+    """The Answer of Life, the Universe, and Everything"""
     return 42
 
 def  生命と宇宙そして万物についての究極の疑問の答え ():
+    """The Answer to the Ultimate Question
+    of Life, the Universe, and Everything"""
     return 42
 
 
